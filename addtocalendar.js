5f4a7759e7ed7fcd57d38439ea2a3b508aa14b0d
Nicolai8 <bolotin.nicolai@gmail.com>
2016-08-26 15:22:35 +0300

    include license, add IE8 support

--- addtocalendar.js
+++ addtocalendar.js
@@ -1,3 +1,26 @@
+/* http://addtocalendar.com/
+ *
+ *
+ * @license
+ The MIT License (MIT)
+ Copyright (c) 2015 AddToCalendar
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
 (function (w, d) {
     var
         atc_url = '//addtocalendar.com/atc/',
@@ -65,7 +88,8 @@
                     var k = false;
                     try {
                         k = w.frameElement == null
-                    } catch (j) {}
+                    } catch (j) {
+                    }
                     if (b.doScroll && k) ie()
                 } else {
                     old = w.onload;
@@ -76,6 +100,7 @@
                 }
             }
         }
+
         if (d.addEventListener) {
             done = function () {
                 d.removeEventListener("DOMContentLoaded", done, false);
@@ -102,6 +127,7 @@
             }
             go()
         }
+
         return function (callback) {
             check();
             if (inited) {
@@ -130,47 +156,47 @@
         'zh': '添加到日历'
     };
 
-    addtocalendar.calendar_urls = {
+    addtocalendar.calendar_urls = {}
 
-    }
-
-    addtocalendar.loadSettings = function(element){
+    addtocalendar.loadSettings = function (element) {
         var settings = {
-            'language':'auto',
-            'show-list-on':'click',
-            'calendars':[
+            'language': 'auto',
+            'show-list-on': 'click',
+            'calendars': [
                 'iCalendar',
                 'Google Calendar',
                 'Outlook',
                 'Outlook Online',
                 'Yahoo! Calendar'
             ],
-            'secure':'auto',
-            'on-button-click':function(){},
-            'on-calendar-click':function(){}
+            'secure': 'auto',
+            'on-button-click': function () {
+            },
+            'on-calendar-click': function () {
+            }
         };
 
-        for (var option in settings){
+        for (var option in settings) {
             var pname = 'data-' + option;
             var eattr = element.getAttribute(pname);
-            if(eattr != null){
+            if (eattr != null) {
 
-                if(isArray(settings[option])){
-                    settings[option] = eattr.replace(/\s*,\s*/g,',').replace(/^\s+|\s+$/g, '').split(',');
+                if (isArray(settings[option])) {
+                    settings[option] = eattr.replace(/\s*,\s*/g, ',').replace(/^\s+|\s+$/g, '').split(',');
                     continue;
                 }
 
-                if(isFunc(settings[option])){
+                if (isFunc(settings[option])) {
                     var fn = window[eattr];
-                    if(isFunc(fn)) {
-                        settings[option]=fn;
-                    }else {
-                        settings[option]=eval('(function(mouseEvent){'+eattr+'})');
+                    if (isFunc(fn)) {
+                        settings[option] = fn;
+                    } else {
+                        settings[option] = eval('(function(mouseEvent){' + eattr + '})');
                     }
                     continue;
                 }
 
-                settings[option]=element.getAttribute(pname);
+                settings[option] = element.getAttribute(pname);
             }
         }
 
@@ -180,11 +206,11 @@
     addtocalendar.load = function () {
 
         var calendarsUrl = {
-            'iCalendar':'ical',
-            'Google Calendar':'google',
-            'Outlook':'outlook',
-            'Outlook Online':'outlookonline',
-            'Yahoo! Calendar':'yahoo'
+            'iCalendar': 'ical',
+            'Google Calendar': 'google',
+            'Outlook': 'outlook',
+            'Outlook Online': 'outlookonline',
+            'Yahoo! Calendar': 'yahoo'
         };
         var utz = (-(new Date()).getTimezoneOffset().toString());
 
@@ -199,26 +225,26 @@
                 var settings = addtocalendar.loadSettings(dom[tagnum]);
 
                 var protocol = 'http:';
-                if(settings['secure'] == 'auto'){
+                if (settings['secure'] == 'auto') {
                     protocol = location.protocol == 'https:' ? 'https:' : 'http:';
-                } else if(settings['secure'] == 'true'){
+                } else if (settings['secure'] == 'true') {
                     protocol = 'https:';
                 }
 
                 var tag_id = dom[tagnum].id;
                 var atc_button_title = languages['en'];
-                if(settings['language'] == 'auto'){
+                if (settings['language'] == 'auto') {
                     var user_lang = "no_lang";
-					if (typeof navigator.language === "string") {
-					    user_lang = navigator.language.substr(0, 2)
-					} else if (typeof navigator.browserLanguage === "string") {
-					    user_lang = navigator.browserLanguage.substr(0, 2)
-					}
+                    if (typeof navigator.language === "string") {
+                        user_lang = navigator.language.substr(0, 2)
+                    } else if (typeof navigator.browserLanguage === "string") {
+                        user_lang = navigator.browserLanguage.substr(0, 2)
+                    }
 
-                    if(languages.hasOwnProperty(user_lang)){
+                    if (languages.hasOwnProperty(user_lang)) {
                         atc_button_title = languages[user_lang];
                     }
-                }else if(languages.hasOwnProperty(settings['language'])){
+                } else if (languages.hasOwnProperty(settings['language'])) {
                     atc_button_title = languages[settings['language']];
                 }
 
@@ -231,50 +257,51 @@
                 var addtocalendar_div = dom[tagnum].getElementsByTagName('var');
                 var event_number = -1;
                 for (var varnum = 0; varnum < addtocalendar_div.length; varnum++) {
-                    var param_name = addtocalendar_div[varnum].className.replace("atc_","").split(" ")[0];
+                    var param_name = addtocalendar_div[varnum].className.replace("atc_", "").split(" ")[0];
                     var param_value = addtocalendar_div[varnum].innerHTML;
 
-                    if(param_name == 'event'){
+                    if (param_name == 'event') {
                         event_number++;
                         continue;
                     }
 
-                    if(param_name == addtocalendar_div[varnum].className){
-                        if(param_name == 'atc-body'){
+                    if (param_name == addtocalendar_div[varnum].className) {
+                        if (param_name == 'atc-body') {
                             atc_button_title = param_value;
                         }
                         continue;
                     }
 
-                    if(event_number == -1){
+                    if (event_number == -1) {
                         continue;
                     }
 
-                    url_paramteres.push('e['+event_number+']['+param_name+']' + '=' + encodeURIComponent(param_value));
+                    url_paramteres.push('e[' + event_number + '][' + param_name + ']' + '=' + encodeURIComponent(param_value));
                 }
 
 
-                var atcb_link_id_val = (tag_id == ''?'':(tag_id + '_link') );
+                var atcb_link_id_val = (tag_id == '' ? '' : (tag_id + '_link') );
                 var atcb_list = document.createElement('ul');
                 atcb_list.className = 'atcb-list';
 
                 var menu_links = '';
-                for (var cnum in settings['calendars']){
-                    if(!calendarsUrl.hasOwnProperty(settings['calendars'][cnum])){
+                for (var cnum in settings['calendars']) {
+                    if (!calendarsUrl.hasOwnProperty(settings['calendars'][cnum])) {
                         continue;
                     }
                     var cal_id = calendarsUrl[settings['calendars'][cnum]];
-                    var atcb_cal_link_id = (tag_id == '' ? '' : ('id="'+tag_id + '_' + cal_id + '_link"') );
-                    menu_links += '<li class="atcb-item"><a '+atcb_cal_link_id+' class="atcb-item-link" href="' 
-						+ (cal_id=='ical' && /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream ? 'webcal:' : protocol)
-						+ atc_url
-						+ cal_id + '?' + url_paramteres.join('&')
-						+ '" target="_blank">' + settings['calendars'][cnum] + '</a></li>';
+                    var atcb_cal_link_id = (tag_id == '' ? '' : ('id="' + tag_id + '_' + cal_id + '_link"') );
+                    menu_links += '<li class="atcb-item"><a ' + atcb_cal_link_id + ' class="atcb-item-link" href="'
+                        + (cal_id == 'ical' && /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream ? 'webcal:' : protocol)
+                        + atc_url
+                        + cal_id + '?' + url_paramteres.join('&')
+                        + '" target="_blank">' + settings['calendars'][cnum] + '</a></li>';
                 }
                 atcb_list.innerHTML = menu_links;
 
-                if(dom[tagnum].getElementsByClassName('atcb-link')[0] == undefined){
-                    var atcb_link = document.createElement('a');
+                var atcb_link;
+                if (dom[tagnum].querySelector('.atcb-link') == undefined) {
+                    atcb_link = document.createElement('a');
                     atcb_link.className = 'atcb-link';
                     atcb_link.innerHTML = atc_button_title;
                     atcb_link.id = atcb_link_id_val;
@@ -282,25 +309,32 @@
 
                     dom[tagnum].appendChild(atcb_link);
                     dom[tagnum].appendChild(atcb_list);
-                }else{
-                    var atcb_link = dom[tagnum].getElementsByClassName('atcb-link')[0];
+                } else {
+                    atcb_link = dom[tagnum].querySelector('atcb-link');
                     atcb_link.parentNode.appendChild(atcb_list);
-                    atcb_link.tabIndex=1;
-                    if(atcb_link.id == ''){
+                    atcb_link.tabIndex = 1;
+                    if (atcb_link.id == '') {
                         atcb_link.id = atcb_link_id_val;
                     }
                 }
 
-                dom[tagnum]
-                    .getElementsByClassName('atcb-link')[0]
-                    .addEventListener("click", settings['on-button-click'], false);
-
-                var item_links = dom[tagnum].getElementsByClassName('atcb-item-link');
-
-                for (var varnum = 0; varnum < item_links.length; varnum++) {
-                    item_links[varnum].addEventListener("click", settings['on-calendar-click'], false);
+                if (atcb_link.addEventListener) {
+                    atcb_link.addEventListener('click', settings['on-button-click'], false);
+                } else {
+                    atcb_link.attachEvent('onclick', settings['on-button-click']);
                 }
 
+                var item_links = dom[tagnum].querySelectorAll('atcb-item-link');
+
+                for (var varnum = 0; varnum < item_links.length; varnum++) {
+                    var item_link = item_links[varnum];
+                    if (item_link.addEventListener) {
+                        item_link.addEventListener('click', settings['on-calendar-click'], false);
+                    }else{
+                        item_link.attachEvent('onclick', settings['on-calendar-click']);
+                    }
+
+                }
             }
         }
     };
